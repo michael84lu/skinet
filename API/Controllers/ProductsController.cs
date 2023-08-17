70c3533540491d32ce6d1517fdd78820a5471e2e
Michael Lu <michael84lu@hotmail.com>
2023-08-17 16:03:42 +0300

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -23,6 +23,20 @@ namespace API.Controllers
             return Ok(products);
         }
 
+        [HttpGet("types")]
+        public async Task<ActionResult<List<ProductType>>> GetProductTypes()
+        {
+            var productTypes = await _repository.GetProductTypesAsync();
+            return Ok(productTypes);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
+        {
+            var productBrands = await _repository.GetProductBrandAsync();
+            return Ok(productBrands);
+        }
+
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
